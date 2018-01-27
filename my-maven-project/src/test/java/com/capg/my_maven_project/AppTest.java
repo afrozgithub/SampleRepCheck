0b37850221e77f343bf67a3f1268e623f1fa4d10
afrozgithub <afroz.melshop@gmail.com>
2018-01-27 22:53:10 +1100

    added few code

---
+++ my-maven-project/src/test/java/com/capg/my_maven_project/AppTest.java
@@ -0,0 +1,18 @@
+package com.capg.my_maven_project;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest
+{
+
+	@Test
+	public  void sample()
+	{
+		System.out.println("hi");
+	}
+
+}
